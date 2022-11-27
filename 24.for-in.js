63cae39d01b9d2b6b431040d26efbaf781f62dc3
Priydarshini Karanth <kpriya1203@gmail.com>
2022-11-27 21:42:09 +0530

    for in loop

--- 24.for-in.js
+++ 24.for-in.js
@@ -1,5 +1,6 @@
-// for-in loop
-// used to traverse the object
+// "for-in" loop - iterate over object properties
+// not advised to use it on arrays, especially if the order is important
+// on arrays use "for-of" loop instead
 
 const person = {
   name: 'John',
